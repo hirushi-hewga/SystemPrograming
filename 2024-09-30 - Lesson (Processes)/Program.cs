1201fddd93712cf25d6d7ebe1e4d93f4c6be3462
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-11-28 10:02:02 -0800

    Add project files.

---
+++ 2024-09-30 - Lesson (Processes)/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+using static System.Console;
+
+namespace _2024_09_30___Lesson__Processes_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process pr = new Process();
+
+            #region Working with Current Process
+
+            //Process processes = new Process();
+
+            Process current = Process.GetCurrentProcess();
+
+            //Process Proirity:
+            // * Idle
+            // * BelowNormal
+            // * Normal(def)
+            // * AboveNormal
+            // * High
+            // * RealTime(only set by OS)
+            // */
+
+            current.PriorityClass = ProcessPriorityClass.High;
+
+            //////////////////////// Process Info
+            WriteLine("----------- Current Process Info -----------");
+            WriteLine("Priority class : " + current.PriorityClass);
+            WriteLine("Name : " + current.ProcessName);
+            WriteLine("Id : " + current.Id);
+            WriteLine("Machine name : " + current.MachineName);
+            WriteLine("Private memory size (KB) : " + current.PrivateMemorySize64 / 1024);
+            WriteLine("Start time : " + current.StartTime);
+            WriteLine("Total processor time : " + current.TotalProcessorTime);
+            WriteLine("User processor time : " + current.UserProcessorTime);
+
+            #endregion
+        }
+    }
+}
