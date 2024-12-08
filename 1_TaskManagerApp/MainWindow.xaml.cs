f37cf8261c51ef4a166d382242f2158968f8ff26
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-12-08 09:55:08 -0800

    TaskManager

---
+++ 1_TaskManagerApp/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _1_TaskManagerApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadProcesses();
+        }
+
+        public void LoadProcesses()
+        {
+            grid.ItemsSource = Process.GetProcesses();
+        }
+
+        private void Refresh_Button_Click(object sender, RoutedEventArgs e)
+        {
+            LoadProcesses();
+        }
+
+        private void Kill_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Process selected = grid.SelectedItem as Process;
+            if (selected == null)
+            {
+                MessageBox.Show("Process is not selected");
+                return;
+            }
+            selected.Kill();
+            MessageBox.Show("Process is killed");
+        }
+
+        private void Close_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ShowInfo_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void StartProcess_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
