b7222c4a4f6ff0f23e54da49fa3dea4daa0837c7
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-12-09 12:48:47 -0800

    Threads

---
+++ 2024-10-02 - Lesson (Threads)/Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Console;
+
+namespace _2024_10_02___Lesson__Threads_
+{
+    class Program
+    {
+        static void Method()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                WriteLine($"\t\t\t{i} - Hello in thread");
+                Thread.Sleep(50);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
