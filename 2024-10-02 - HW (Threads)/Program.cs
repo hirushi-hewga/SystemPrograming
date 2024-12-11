101de99ee33c6e2e306b7ab7869364777dd89ac7
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-12-11 05:15:32 -0800

    Threads

---
+++ 2024-10-02 - HW (Threads)/Program.cs
@@ -0,0 +1,34 @@
+﻿using static System.Console;
+
+namespace _2024_10_02___HW__Threads_
+{
+    internal class Program
+    {
+
+        static void Method()
+        {
+            for (int i = 1; i <= 50; i++)
+            {
+                WriteLine(i);
+                Thread.Sleep(100);
+            }
+        }
+
+        static void Method2()
+        {
+            for (int i = 1; i <= 50; i++)
+            {
+                WriteLine(i);
+                Thread.Sleep(100);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            /// 1
+            Thread thread = new Thread(Method);
+            thread.Start();
+
+        }
+    }
+}
---
+++ 2_ProgressBarApp/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _2_ProgressBarApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Thread thread;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Start_Button_Click(object sender, RoutedEventArgs e)
+        {
+            //HardWork();
+            thread = new Thread(HardWork);
+            thread.Start();
+        }
+
+        public void HardWork()
+        {
+            bool isContinue = false;
+            Application.Current.Dispatcher.Invoke(new Action(() =>
+            {
+                if (progress.Value > 0)
+                    progress.Value = progress.Minimum;
+                isContinue = progress.Value < progress.Maximum;
+            }));
+            while (isContinue)
+            {
+                Application.Current.Dispatcher.Invoke(new Action(() =>
+                {
+                    progress.Value++;
+                    isContinue = progress.Value < progress.Maximum;
+                }));
+                Thread.Sleep(110);
+            }
+        }
+    }
+}
