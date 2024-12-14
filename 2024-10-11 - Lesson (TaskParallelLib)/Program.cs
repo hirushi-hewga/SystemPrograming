ed28bb72e6d8faac04639a0e12bb86383c0d4805
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-12-14 13:15:25 -0800

    TaskParallelLib

---
+++ 2024-10-11 - Lesson (TaskParallelLib)/Program.cs
@@ -0,0 +1,186 @@
+﻿namespace _2024_10_11___Lesson__TaskParallelLib_
+{
+    #region Parallel
+
+    /*
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.Invoke(
+                Display,
+                () => {
+                    Console.WriteLine($"Task executing {Task.CurrentId}");
+                    Thread.Sleep(3000);
+                    Console.WriteLine($"Task ended  {Task.CurrentId}");
+                },
+                () => Factorial(5));
+
+            Console.ReadLine();
+        }
+
+        static void Display()
+        {
+            Console.WriteLine($"Task executing {Task.CurrentId}");
+            Thread.Sleep(3000);
+            Console.WriteLine($"Task ended {Task.CurrentId}");
+        }
+
+        static void Factorial(int x)
+        {
+            int result = 1;
+
+            for (int i = 1; i <= x; i++)
+            {
+                result *= i;
+            }
+            Console.WriteLine($"Task executing {Task.CurrentId}");
+            Thread.Sleep(3000);
+            Console.WriteLine($"Result {result}");
+        }
+    }
+    */
+
+    #endregion
+
+    #region ParallelFor
+
+    /*
+    class Program
+    {
+        static Random rnd = new Random();
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+            Console.WriteLine("Started!");
+
+            //for (int i = 0; i < 20; i++)
+            //{
+            //    Factorial(i);
+            //}
+
+            //for (int i = 1; i < 20; i++)
+            //{
+            //    int n = i;
+            //    Task.Run(() => Factorial(n));
+            //}
+
+            // itertions: [1...19]
+            Parallel.For(1, 20, Factorial);
+
+            Console.ReadLine();
+        }
+
+        static void Factorial(int x)
+        {
+            Random r = new Random();
+            for (int i = 0; i < 100_000_000; i++)
+            {
+                r.Next();
+                r.GetHashCode();
+                r.NextDouble();
+            }
+
+            int result = 1;
+            for (int i = 1; i <= x; i++)
+            {
+                result *= i;
+            }
+
+            Console.WriteLine($"Task executing {Task.CurrentId}");
+            //Thread.Sleep(3000);
+            Console.WriteLine($"Factorial {x} = {result}");
+        }
+    }
+    */
+
+    #endregion
+
+    #region ParallelForeach
+
+    /**/
+    class Program
+    {
+        static Random rnd = new Random();
+        public class Author
+        {
+            public string Name { get; set; }
+            public int[] ratings;
+            public Author(string name = "no name")
+            {
+                Name = name;
+                ratings = new int[5]
+                {
+                    rnd.Next(6),
+                    rnd.Next(6),
+                    rnd.Next(6),
+                    rnd.Next(6),
+                    rnd.Next(6)
+                };
+            }
+        }
+        static void Main(string[] args)
+        {
+            //var list = new List<int>() { 1, 3, 5, 8 };
+            //Parallel.ForEach(list, Factorial);
+
+            List<Author> authors = new List<Author>()
+            {
+                new Author("William"),
+                new Author("Roberto"),
+                new Author("Harry"),
+                new Author("William"),
+                new Author("Roberto"),
+                new Author("Harry"),
+                new Author("Roberto"),
+                new Author("William"),
+                new Author("Roberto"),
+                new Author("Harry"),
+                new Author("Roberto"),
+                new Author("William"),
+                new Author("William"),
+                new Author("William")
+            };
+
+            //foreach (var a in authors)
+            //{
+            //    AverageRating(a);
+            //}
+
+            //Parallel.ForEach(authors, AverageRating);
+
+            DateTime start = DateTime.Now;
+            ParallelLoopResult result = Parallel.ForEach(authors, AverageRating);
+
+            if (result.IsCompleted)
+            {
+                Console.WriteLine("Duration: " + (DateTime.Now - start).TotalSeconds);
+                Console.WriteLine("All Tasks had comple ted!");
+            }
+
+            Console.ReadLine();
+        }
+
+        static void AverageRating(Author author)
+        {
+            var avg = author.ratings.Average();
+            Thread.Sleep(3000);
+            Console.WriteLine($"Author {author.Name} average rating = {avg}");
+        }
+        static void Factorial(int x)
+        {
+            int result = 1;
+
+            for (int i = 1; i <= x; i++)
+            {
+                result *= i;
+            }
+            Console.WriteLine($"Task executing {Task.CurrentId}");
+            Thread.Sleep(3000);
+            Console.WriteLine($"Factorial {x} = {result}");
+        }
+    }
+
+
+    #endregion
+}
