c3e826db70d64505efce033daa013e71502974c4
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-12-15 11:32:52 -0800

    Unsafe Code

---
+++ 2024-10-14 - Lesson (Unsafe Code. Registry)/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _2024_10_14___Lesson__Unsafe_Code._Registry_
+{
+    #region stackalloc
+
+    /**/
+    class Program
+    {
+        // Project -> Properties -> Build -> Allow Unsafe Code
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+
+    #endregion
+}
